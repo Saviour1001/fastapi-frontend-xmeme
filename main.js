30ffb6376a8138f0e69164137278cad2e238fbe3
Saviour1001 <harshghodkar@gmail.com>
2021-02-14 16:52:30 +0530

    Update

--- main.js
+++ main.js
@@ -20,10 +20,12 @@ const getData = () => {
       const html = data.map((user) => {
         return `
         <div class="post">
+        <div class="inner" class="background1">
         <p> Name : ${user.name} </p>
         <p> ${user.caption}</p>
         <img src = "${user.img_meme}"  alt="Your meme" 
-        width = "500"> 
+        width = "500">
+        </div>
         </div>`;
       });
       document.querySelector("#pics").insertAdjacentHTML("afterbegin", html);
